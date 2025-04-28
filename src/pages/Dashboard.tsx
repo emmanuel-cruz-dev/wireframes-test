89321a43daa8b99163a8151a7835a45e7ebd88b4
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-04-28 17:51:56 -0300

    style: estructura en Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,15 +4,20 @@ const Dashboard = () => {
   const { user, logout } = useAuth();
 
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold">Bienvenido, {user?.email}!</h1>
-      <button
-        onClick={logout}
-        className="mt-4 bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded cursor-pointer"
-      >
-        Cerrar sesión
-      </button>
-    </div>
+    <section className="p-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl font-bold">Bienvenido {user?.email}!</h1>
+        <button
+          onClick={logout}
+          className="mt-4 bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded cursor-pointer"
+        >
+          Cerrar sesión
+        </button>
+      </div>
+      <div>
+        <h2>Home</h2>
+      </div>
+    </section>
   );
 };
 
