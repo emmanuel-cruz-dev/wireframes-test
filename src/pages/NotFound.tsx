d0c230125a44cc7c3046f0312259c09c2548bcce
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-05-05 11:42:54 -0300

    feat: vercel.json agregado en la raíz del proyecto

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -6,8 +6,19 @@ export default function NotFound() {
   const [countdown, setCountdown] = useState(10);
 
   useEffect(() => {
-    const timer =
-      countdown > 0 && setInterval(() => setCountdown(countdown - 1), 1000);
+    // const timer =
+    //   countdown > 0 && setInterval(() => setCountdown(countdown - 1), 1000);
+
+    // if (countdown === 0) {
+    //   navigate("/");
+    // }
+
+    // return () => clearInterval(timer);
+    let timer: number | undefined;
+
+    if (countdown > 0) {
+      timer = setInterval(() => setCountdown(countdown - 1), 1000);
+    }
 
     if (countdown === 0) {
       navigate("/");
