57a016f0312c3ace985c37186edac61ffafcaafe
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-24 16:50:39 -0300

    style: updates width in src/pages/Products/Products.tsx

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -2,12 +2,8 @@ import ProductsTable from "../../components/ui/ProductsTable";
 
 function Products() {
   return (
-    <section className="h-full w-full">
-      <nav className="h-18">Navbar</nav>
-      <main className="flex justify-between">
-        <aside className="w-64">Sidebar</aside>
-        <ProductsTable />
-      </main>
+    <section className="h-full w-full bg-custom-mist">
+      <ProductsTable />
     </section>
   );
 }
