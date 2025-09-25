08b22cea36521159c67f4b1c3c41225ba814084f
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-09-25 08:06:59 -0300

    feat: removes PrivateRoutes from AppRoutes in src/routes/index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NotFound from "../pages/NotFound";
 import DashboardPage from "../pages/dashboard/DashboardPage";
-// import LoginPage from "../pages/auth/LoginPage";
-// import RegisterPage from "../pages/auth/RegisterPage";
+import LoginPage from "../pages/auth/LoginPage";
+import RegisterPage from "../pages/auth/RegisterPage";
 import PatientsPage from "../pages/patients/PatientsPage";
 import PublicRoutes from "./PublicRoutes";
-import PrivateRoutes from "./PrivateRoutes";
 import ProfilePage from "../pages/profile/ProfilePage";
 import AppointmentsPage from "../pages/appointments/AppointmentsPage";
 import DashboardPageTwo from "../pages/dashboard/DashboardPageTwo";
@@ -17,18 +16,16 @@ export const AppRoutes = () => (
     <Routes>
       <Route element={<PublicRoutes />}>
         <Route path="/" element={<Products />} />
-        {/* <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} /> */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
 
-      <Route element={<PrivateRoutes />}>
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/dashboard2" element={<DashboardPageTwo />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/patients" element={<PatientsPage />} />
-        <Route path="/appointments" element={<AppointmentsPage />} />
-        <Route path="/patient-tabs" element={<PatientTabs />} />
-      </Route>
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/dashboard2" element={<DashboardPageTwo />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/patients" element={<PatientsPage />} />
+      <Route path="/appointments" element={<AppointmentsPage />} />
+      <Route path="/patient-tabs" element={<PatientTabs />} />
 
       <Route path="/404" element={<NotFound />} />
 
